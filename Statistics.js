beb514015c812f5563d8062de43084144a160a2e
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-08-01 15:21:15 +0200

    fixed

--- Statistics.js
+++ Statistics.js
@@ -1,6 +1,6 @@
 class Statistics {
  constructor(){
-  this.gameResults = [{win: true, bid: 20}, {win: false, bid: -10}, {win: true, bid: 10}];
+  this.gameResults = [];
  }
  addGameToStatistics(win, bid){
   let gameResult = {
